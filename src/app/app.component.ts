87f6004606172fa2ac2d4a3e21100f4bd9626407
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-26 17:47:38 +0000

    Impart todo component into the main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
